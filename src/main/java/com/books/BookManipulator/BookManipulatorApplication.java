0ed37639671070403b9073c7d5fb56cb84a3c8f2
Tsvetan Trifonov <tsmtr_vd@abv.bg>
2020-03-17 00:32:07 -0700

    Initial Commit from initializr

---
+++ src/main/java/com/books/BookManipulator/BookManipulatorApplication.java
@@ -0,0 +1,13 @@
+package com.books.BookManipulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManipulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManipulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/books/BookManipulator/BookManipulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.BookManipulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManipulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
