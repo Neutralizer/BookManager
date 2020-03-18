1238a258b2c107e2df1ed5017996ce10e7b5b45f
Neutralizer <neutralizerradioactive@gmail.com>
2020-03-18 01:35:27 -0700

    project restructuring and setting up temp controller

---
+++ src/main/java/com/books/BookManipulator/controllers/BookController.java
@@ -0,0 +1,16 @@
+package com.books.BookManipulator.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+
+    @GetMapping(path = "/book")
+    public ResponseEntity<String> greeting(){
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+}
