4bf3d9e3047c66375ec6ad378744e23468c97894
Neutralizer <neutralizerradioactive@gmail.com>
2020-03-30 00:19:23 -0700

    Comments improving

--- src/main/java/com/books/controllers/BookController.java
+++ src/main/java/com/books/controllers/BookController.java
@@ -77,7 +77,7 @@ public class BookController {
     }
 
     /**
-     * Increment the rating of a book
+     * Increment the rating of a book.
      * @param id the id of the book
      * @return Status OK if successful.
      */
